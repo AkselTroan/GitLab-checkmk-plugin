c354dc627c44bf0fdc3339447d0d7baf23a9abc8
Aksel Trøan <49494113+AkselTroan@users.noreply.github.com>
2022-03-12 17:10:48 +0100

    First version. Not tested

---
+++ mk_gitlab.py
@@ -0,0 +1,34 @@
+import requests, sys, json
+
+
+__version__ = "2.0.0p20"
+
+ip = "localhost"
+port = "443"
+server = ip + ":" + port
+
+
+health = requests.get(server + "/-/health")
+readiness = requests.get(server + "/-/readiness?all=1")
+liveness = requests.get(server + "/-/liveness")
+
+if health.text == "Gitlab OK":
+    sys.stdout.write('0 "Health" - Health passed')
+else:
+    sys.stdout.write('2 "Health" - Health failed')
+
+
+read = json.loads(readiness.text)
+live = json.loads(liveness.text)
+
+
+if read['status'] == "ok":
+    sys.stdout.write('0 "Readiness" - Readiness passed')
+else:
+    sys.stdout.write('2 "Readiness" - Readiness failed')
+
+
+if live['status'] == "ok":
+    sys.stdout.write('0 Liveness" - Liveness passed')
+else:
+    sys.stdout.write('2 "Liveness" - Liveness Failed')
